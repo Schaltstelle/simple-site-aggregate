8906ca3ddaee8b12a12e015f920d79a8a9bc5d9c
Stefan Niederhauser <ghuder5@gmx.ch>
2019-06-10 19:59:24 +0200

    create parsed yaml file, remove template map

--- src/index.js
+++ src/index.js
@@ -1,139 +1,113 @@
-'use strict';
-const debug = require('debug')('aggregate');
-const chalk = require('chalk');
-const fs = require('fs');
-const fse = require('fs-extra');
-const path = require('path');
-const request = require('request');
-const url = require('url');
-const cheerio = require('cheerio');
-const moment = require('moment');
-const ss = require('simple-site');
+'use strict'
+const debug = require('debug')('aggregate')
+const chalk = require('chalk')
+const fs = require('fs')
+const fse = require('fs-extra')
+const path = require('path')
+const request = require('request')
+const url = require('url')
+const cheerio = require('cheerio')
+const moment = require('moment')
+const ss = require('simple-site')
+const yaml = require('js-yaml')
 
-let parseFuncs = {};
+let parseFuncs = {}
 
 registerParser('parseDate', (data, format) => {
-    return moment(data, format).toDate();
-});
+    return moment(data, format).toDate()
+})
 
-ss.registerHelper('aggregate', run);
+ss.registerHelper('aggregate', run)
 
 module.exports = {
     run: run,
     registerParser: registerParser
-};
+}
 
 function registerParser(name, func) {
-    parseFuncs[name] = func;
+    parseFuncs[name] = func
 }
 
 /**
  * @param url The URL to fetch data
  * @param parser The parser file to use
  * @param templ The output template to use. May be an object {outputFile: template...}
- * @param maxLen Max length of data to be fetched, if it's longer, it will be truncated at </p>
  * @param config Additional data given to the template
  * @returns {*}
  */
-function run(url, parser, templ, maxLen, config) {
-    if (!config) {
-        config = maxLen;
-        maxLen = 0;
-    }
-    return doRun(url, parser, templ, maxLen, config ? config.hash : {});
+function run(url, parser, templ, config) {
+    return doRun(url, parser, templ, config ? config.hash : {})
 }
 
-function doRun(_url, _parser, _templ, maxLen, config) {
+function doRun(_url, _parser, _templ, config) {
     return resolveArgs(_url, _parser, _templ, config).then(params => {
-        let [url, parserName, templ] = params;
-        let cacheDir = '_work';
+        let [url, parserName, templ] = params
+        let cacheDir = '_work/aggregate/' + (config.context || 'default')
+        let dataDir = cacheDir + '/parsed'
         if (ss.configs.clean) {
-            fse.removeSync(cacheDir);
+            fse.removeSync(cacheDir)
         }
-        fse.mkdirsSync(cacheDir);
-        debug('Searching', chalk.blue(url));
+        fse.mkdirsSync(dataDir)
+        debug('Searching', chalk.blue(url))
         let parser = findParser(parserName)
-        let cache = path.resolve(cacheDir, filenameSafe(url));
-        let doLoad = fs.existsSync(cache) ? readFile(cache) : load(url, ss.configs.outputDir, parser);
+        let cache = path.resolve(cacheDir, filenameSafe(url))
+        let doLoad = fs.existsSync(cache) ? readFile(cache) : load(url, ss.configs.outputDir, parser)
         return doLoad.then(data => {
-            fs.writeFileSync(cache, data);
-            let info = parse(url, data, parser, maxLen);
-            let context = Object.assign(info, config);
-            return Promise.all(templatePromises(templ, context)).then(res => {
-                let nonEmpty = res.filter(r => r);
-                return nonEmpty.length === 0 ? '' : nonEmpty[0];
-            });
-        });
-    });
+            fs.writeFileSync(cache, data)
+            let info = parse(url, data, parser, config.maxLen || 0)
+            let context = Object.assign(info, config)
+            fs.writeFileSync(path.resolve(dataDir, filenameSafe(url) + '.yaml'), yaml.safeDump(context))
+            return execTemplate(fs.readFileSync(templ, 'utf8'), context)
+        })
+    })
 }
 
 function resolveArgs(url, parser, templ, config) {
     return Promise.all([
         ss.template(url, config).then(res => res.data),
         ss.template(parser, config).then(res => res.data),
-        ss.template(templ, config).then(res => res.data)]);
+        ss.template(templ, config).then(res => res.data)])
 }
 
-function templatePromises(templ, context) {
-    let templates = templ[0] === '{' ? ss.loadYamlSync(templ) : {[templ]: null};
-    let promises = [];
-    for (let p in templates) {
-        if (!templates[p]) {
-            templates[''] = p;
-            p = '';
-        }
-        promises.push(execTemplate(fs.readFileSync(templates[p], 'utf8'), context, p));
-    }
-    return promises;
-}
-
-function execTemplate(file, context, target) {
-    return ss.template(file, context).then(res => {
-        if (target) {
-            fse.mkdirsSync(path.dirname(target));
-            fs.writeFileSync(target, res.data);
-            debug('Wrote', chalk.blue(target));
-        } else {
-            return res.data;
-        }
-    });
+function execTemplate(file, context) {
+    return ss.template(file, context).then(res => res.data)
 }
 
 function filenameSafe(s) {
-    return s.replace(/[/\\:*?"<>|]/g, '-');
+    return s.replace(/[/\\:*?"<>|]/g, '-')
 }
 
 function readFile(file) {
     return new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, data) => {
             if (err) {
-                reject(err);
+                reject(err)
             } else {
-                debug('Found', chalk.green(path.relative('', file)));
-                resolve(data);
+                debug('Found', chalk.green(path.relative('', file)))
+                resolve(data)
             }
         })
-    });
+    })
 }
 
-let parsers = {};
+let parsers = {}
 
 function findParser(parser) {
     if (!parsers[parser]) {
-        parsers[parser] = ss.loadYamlSync(fs.readFileSync(parser, 'utf8'));
+        parsers[parser] = ss.loadYamlSync(fs.readFileSync(parser, 'utf8'))
     }
-    return parsers[parser];
+    return parsers[parser]
 }
 
 function load(addr, baseDir, parser, count) {
     if (count > 5) {
-        return Promise.reject('Too many retries');
+        return Promise.reject('Too many retries')
     }
     if (addr.substring(0, 4) !== 'http') {
-        let filename = path.resolve(baseDir, addr);
-        return retrying(readFile(filename));
+        let filename = path.resolve(baseDir, addr)
+        return retrying(readFile(filename))
     }
-    return retrying(get(addr));
+    return retrying(get(addr))
 
     function retrying(promise) {
         return promise
@@ -154,101 +128,101 @@ function load(addr, baseDir, parser, count) {
 }
 
 function parse(addr, data, template, maxLen) {
-    const tags = cheerio.load(data);
-    let info = Object.assign({url: addr}, template.static);
+    const tags = cheerio.load(data)
+    let info = Object.assign({url: addr}, template.static)
     for (let select in template.selectors) {
-        let value = applySelector(addr, tags, template.selectors[select], maxLen);
+        let value = applySelector(addr, tags, template.selectors[select], maxLen)
         if (value != null) {
-            info[select] = value;
+            info[select] = value
         }
     }
-    return info;
+    return info
 }
 
 function applySelector(addr, tags, selector, maxLen) {
     if (Array.isArray(selector)) {
-        let applied = selector.slice();
-        applied[1] = extract(addr, tags, selector[1], maxLen);
-        return parseFunc.apply(null, applied);
+        let applied = selector.slice()
+        applied[1] = extract(addr, tags, selector[1], maxLen)
+        return parseFunc.apply(null, applied)
     }
-    return extract(addr, tags, selector, maxLen);
+    return extract(addr, tags, selector, maxLen)
 }
 
 function parseFunc(func, data, params) {
-    let parser = parseFuncs[func];
+    let parser = parseFuncs[func]
     if (!parser) {
-        debug(chalk.red('Ignoring unknown parse function ' + func));
-        return data;
+        debug(chalk.red('Ignoring unknown parse function ' + func))
+        return data
     }
-    return parser.apply(null, Array.prototype.slice.call(arguments, 1));
+    return parser.apply(null, Array.prototype.slice.call(arguments, 1))
 }
 
 function extract(addr, tags, selector, maxLen) {
-    let elem = /(.*?) \[(.*?)]$/.exec(selector);
+    let elem = /(.*?) \[(.*?)]$/.exec(selector)
     if (elem) {
-        let css = elem[1];
-        let attr = elem[2];
-        let tag = tags(css);
+        let css = elem[1]
+        let attr = elem[2]
+        let tag = tags(css)
         if (tag.length === 0) {
             debug(chalk.blue(addr), chalk.red('tag "' + css + '" not found.'))
-            return null;
+            return null
         }
         if (attr.charAt(0) === '.') {
-            return tag.hasClass(attr.substring(1));
+            return tag.hasClass(attr.substring(1))
         }
-        let val = tag.attr(attr);
+        let val = tag.attr(attr)
         if (!val) {
             debug(chalk.blue(addr), chalk.red('attribute "' + attr + '" not found on tag "' + css + '".'))
-            return null;
+            return null
         }
         if (tag.get(0).tagName === 'img') {
             if (attr === 'src') {
-                val = relative(val, addr);
+                val = relative(val, addr)
             } else if (attr === 'srcset') {
-                val = relative(val, addr); //TODO support it correctly
+                val = relative(val, addr) //TODO support it correctly
             }
         }
-        return val;
+        return val
     }
-    let tag = tags(selector);
+    let tag = tags(selector)
     if (tag.length === 0) {
         debug(chalk.blue(addr), chalk.red('tag "' + selector + '" not found.'))
-        return null;
+        return null
     }
-    return convertToHtml(addr, tag, maxLen);
+    return convertToHtml(addr, tag, maxLen)
 }
 
 function convertToHtml(addr, elems, maxLen) {
     elems.find('a').each((i, e) => {
-        let ee = cheerio(e);
-        ee.attr('href', relative(ee.attr('href'), addr));
-    });
-    let val = elems.html();
+        let ee = cheerio(e)
+        ee.attr('href', relative(ee.attr('href'), addr))
+    })
+    let val = elems.html()
     if (maxLen > 0 && val && val.length > maxLen) {
-        let pos = val.indexOf('</p>', maxLen);
+        let pos = val.indexOf('</p>', maxLen)
         if (pos > 0) {
-            val = val.substring(0, pos + 4);
+            val = val.substring(0, pos + 4)
         }
     }
-    return val;
+    return val
 }
 
 function relative(href, base) {
-    let rel = href;
+    let rel = href
     if (href && !href.match('^https?://')) {
         if (href.substring(0, 1) === '#') {
-            rel = base + href;
+            rel = base + href
         } else if (href.substring(0, 1) === '/') {
-            rel = base.substring(0, base.indexOf('/', 8)) + href;
+            rel = base.substring(0, base.indexOf('/', 8)) + href
         } else {
-            rel = base.substring(0, base.lastIndexOf('/') + 1) + href;
+            rel = base.substring(0, base.lastIndexOf('/') + 1) + href
         }
     }
-    return path.normalize(rel).replace(/(https?:\/)([^/])/, '$1/$2');
+    return path.normalize(rel).replace(/(https?:\/)([^/])/, '$1/$2')
 }
 
 function get(addr) {
-    debug('Loading', chalk.green(addr));
+    debug('Loading', chalk.green(addr))
     return new Promise((resolve, reject) => {
         let options = {
             url: addr,
@@ -256,15 +230,15 @@ function get(addr) {
                 accept: 'text/html',
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
             }
-        };
+        }
         request(options, (err, res, body) => {
             if (err) {
-                reject(err);
+                reject(err)
             } else if (res.statusCode !== 200) {
-                reject('Got response code ' + res.statusCode);
+                reject('Got response code ' + res.statusCode)
             } else {
-                resolve(body);
+                resolve(body)
             }
-        });
-    });
+        })
+    })
 }
--- test/test.js
+++ test/test.js
@@ -21,22 +21,15 @@ describe('aggregate', () => {
     });
     it('should gather data from url', () => {
         fse.removeSync('_work');
-        return index.run('http://en.wikipedia.org', 'test/parsers/wikipedia.yaml', 'test/agg-template.html', 0, {hash: {a: 42}}).then(res => {
+        return index.run('http://en.wikipedia.org', 'test/parsers/wikipedia.yaml', 'test/agg-template.html', {hash: {a: 42}}).then(res => {
             assert.equal(res, fs.readFileSync('test/expected-url-agg.html', 'utf8'));
-        });
-    });
-    it('should support a map as output', () => {
-        fse.removeSync('_work');
-        return index.run('../test/agg-input.html', 'test/parsers/file.yaml', '{_work/out.html: test/agg-template.html}').then(res => {
-            assert.equal(res, '');
-            assert.equal(fs.readFileSync('_work/out.html', 'utf8'), fs.readFileSync('test/expected-file-agg.html', 'utf8'));
         });
     });
     it('should work as a helper', () => {
         fse.removeSync('_work');
-        return ss.template('{{{aggregate "{{url}}/agg-input.html" "{{parser}}/file.yaml" "{ {{temp}},_work/bla/out.html: {{temp}} }" url="../test" parser="test/parsers" temp="test/agg-template.html" }}}', {}).then(res => {
+        return ss.template('{{{aggregate "{{url}}/agg-input.html" "{{parser}}/file.yaml" "test/agg-template.html" url="../test" parser="test/parsers" }}}', {}).then(res => {
             assert.equal(res.data, fs.readFileSync('test/expected-file-agg.html', 'utf8'));
-            assert.equal(fs.readFileSync('_work/bla/out.html', 'utf8'), fs.readFileSync('test/expected-file-agg.html', 'utf8'));
+            assert.equal(fs.readFileSync('_work/aggregate/default/parsed/..-test-agg-input.html.yaml', 'utf8'), fs.readFileSync('test/expected-url-agg.yaml', 'utf8'));
         });
     });
 });
